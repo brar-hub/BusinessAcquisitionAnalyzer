829b07c107e7b94b60eefbc85c439e6c37127f57
Brar <sharanbr18@gmail.com>
2026-08-27 00:24:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import plotly.graph_objects as go
 # ============================================================
 
 st.set_page_config(
-    page_title="WTM Business Acquisition Analyzer",
+    page_title="WTM. Business Acquisition Analyzer",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded"
